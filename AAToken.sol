62d6972959df280d61809c90d2084b75f51e3235
zappierRabbit <31389049+zappierRabbit@users.noreply.github.com>
2022-08-22 13:03:29 +0500

    Create AAToken.sol

---
+++ AAToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.7;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract AAToken is ERC20{
+    constructor (string memory _name, string memory _symbol) ERC20(_name, _symbol){
+        _mint(msg.sender, 10 * 10 ** 18);
+    }
+}
